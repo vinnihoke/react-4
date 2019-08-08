f9cd6cfa7e8d5172246de102ab3f69252fed0264
Vinni Hoke <vinni.hoke@gmail.com>
2019-08-07 18:58:16 -0700

    Partial MVP

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
+// React Imports
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom'
+
+// Local Imports
 import './App.css';
 import { Home, About, Contact, Navigation } from './components';
 
-const App = () => (
-  <div>
-    <Navigation />
-  </div>
-);
+const App = () => {
+  return (
+    <BrowserRouter>
+      <div>
+        <Navigation />
+        <Route exact path='/' component={Home} />
+        <Route path='/about' component={About} />
+        <Route path='/contact' component={Contact} />
+      </div>
+    </BrowserRouter>
+)};
 
 export default App;
